1afbece9e01dc9b7e6878e9173bfcd1ae27c9c20
Justyna W <jkwodecka@gmail.com>
2025-02-08 21:55:04 +0100

    add styles

--- src/components/Result.tsx
+++ src/components/Result.tsx
@@ -11,20 +11,20 @@ export const Result = ({ bill, tip, people, handleReset }: ResultProps) => {
     const totalAmount: number = Math.round((bill + bill * (tip / 100)) / people * 100) / 100;
     return (
         <div className="results">
-            <div className="results_text">
-                <div className="sum sum--first">
-                    <div className="amount_text">
-                        <p className="Subtitle_results">Tip Amount</p>
-                        <p className="p_results">/ person</p>
+            <div className="results-text">
+                <div className="sum sum-first">
+                    <div className="amount-text">
+                        <p>Tip Amount</p>
+                        <p className="p-results">/ person</p>
                     </div>
-                    <p className="total_amount" id="tip_amount">{tipAmount}</p>
+                    <p className="total-amount" id="tip-amount">{tipAmount}</p>
                 </div>
                 <div className="sum">
-                    <div className="amount_text">
-                        <p className="Subtitle_result">Total</p>
-                        <p className="p_results">/ person</p>
+                    <div className="amount-text">
+                        <p>Total</p>
+                        <p className="p-results">/ person</p>
                     </div>
-                    <p className="total_amount" id="total">{totalAmount}</p>
+                    <p className="total-amount" id="total">{totalAmount}</p>
                 </div>
             </div>
             <button className="reset" id="reset" onClick={handleReset}>RESET</button>
